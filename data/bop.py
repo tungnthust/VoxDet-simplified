d63d8601d5fe8bac74ad7881f76463b7a0f74408
tungnthust <thanhtungbka1@gmail.com>
2024-03-08 08:30:34 +0700

    add test data conf

--- data/bop.py
+++ data/bop.py
@@ -25,15 +25,15 @@ from terminaltables import AsciiTable
 
 from .custom import CustomDataset
 
-try:
-    import pycocotools
-    if not hasattr(pycocotools, '__sphinx_mock__'):  # for doc generation
-        assert pycocotools.__version__ >= '12.0.2'
-except AssertionError:
-    raise AssertionError('Incompatible version of pycocotools is installed. '
-                         'Run pip uninstall pycocotools first. Then run pip '
-                         'install mmpycocotools to install open-mmlab forked '
-                         'pycocotools.')
+# try:
+#     import pycocotools
+#     if not hasattr(pycocotools, '__sphinx_mock__'):  # for doc generation
+#         assert pycocotools.__version__ >= '12.0.2'
+# except AssertionError:
+#     raise AssertionError('Incompatible version of pycocotools is installed. '
+#                          'Run pip uninstall pycocotools first. Then run pip '
+#                          'install mmpycocotools to install open-mmlab forked '
+#                          'pycocotools.')
 
 LMO_OBJECT = ('1', '5', '6', '8', '9',
             '10', '11', '12')
